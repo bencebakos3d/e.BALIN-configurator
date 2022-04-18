758fe0be0c450ad33120d6910e5a8b61a55b683e
bencebakos3d <bencemail1@gmail.com>
2022-04-18 11:39:13 +0200

    Image switching functionality added

--- public/app.js
+++ public/app.js
@@ -1,11 +1,12 @@
 const biminiInput = document.getElementById("bimini-input");
 const imgBase = document.getElementById("img-base");
+let currentView = 1;
 
 biminiInput.addEventListener("change", () => {
     if (biminiInput.checked) {
-        imgBase.setAttribute("src", "img/ebalin_base.jpg" )
+        imgBase.setAttribute("src", `img/ebalin_base_${currentView}.jpg` )
     } else {
-        imgBase.setAttribute("src", "img/ebalin_base_nobimini.jpg")
+        imgBase.setAttribute("src", `img/ebalin_base_nobimini_${currentView}.jpg`)
     }
 })
 
@@ -29,6 +30,24 @@ fetch('/options').then(response => response.json()).then(data => {
     })
 })
 
-function toggleOptionDetails(element) {
-    
+function switchView(toNext) {
+    if (toNext === true) {
+        if (currentView < 3) {
+            currentView++
+        } else {
+            currentView = 1
+        }
+    } else {
+        if (currentView > 1 ) {
+            currentView--
+        } else {
+            currentView = 3
+        }
+    }
+    const images = document.querySelectorAll(".configurator-left > img");
+    images.forEach(element => {
+        let source = element.getAttribute("src");
+        source = source.substring(0, source.length - 5).concat(currentView + ".jpg");
+        element.setAttribute("src", source)
+    })
 }
