12e38aca89f601a65efdbb1d6d97eb1032e62d6d
Bence Bakos <bencemail1@gmail.com>
2022-06-29 22:51:30 +0200

    Added 3D data, fixed image position and functions

--- public/app.js
+++ public/app.js
@@ -1,53 +1,47 @@
-const biminiInput = document.getElementById("bimini-input");
-const imgBase = document.getElementById("img-base");
-let currentView = 1;
-
-biminiInput.addEventListener("change", () => {
-    if (biminiInput.checked) {
-        imgBase.setAttribute("src", `img/ebalin_base_${currentView}.jpg` )
-    } else {
-        imgBase.setAttribute("src", `img/ebalin_base_nobimini_${currentView}.jpg`)
-    }
-})
+const biminiInput = document.getElementById('bimini-input');
+const imgBase = document.getElementById('img-base');
+let images = document.querySelectorAll('.img-wrapper > img');
+let currentView = 0;
 
 function toggleDropdown(wrapperID) {
-    const menu = document.getElementById(wrapperID)
-    const icon = document.getElementById(wrapperID + "-icon")
-    if (menu.style.display === "none") {
-        menu.style.display = "initial"
-        icon.style.transform = "none"
-    } else {
-        menu.style.display = "none"
-        icon.style.transform = "rotate(180deg)"
-    }
+  const menu = document.getElementById(wrapperID);
+  const icon = document.getElementById(wrapperID + '-icon');
+  if (menu.style.display === 'none') {
+    menu.style.display = 'initial';
+    icon.style.transform = 'none';
+  } else {
+    menu.style.display = 'none';
+    icon.style.transform = 'rotate(180deg)';
+  }
 }
 
-fetch('/options').then(response => response.json()).then(data => {
-    const optionWrappers = document.querySelectorAll('.option');
-    optionWrappers.forEach(option => {
-        const optionID = parseInt(option.getAttribute("databaseID"));
-        option.children[0].innerHTML = data[optionID - 1].name;
-    })
-})
-
 function switchView(toNext) {
-    if (toNext === true) {
-        if (currentView < 3) {
-            currentView++
-        } else {
-            currentView = 1
-        }
+  if (toNext === true) {
+    if (currentView < 2) {
+      currentView++;
     } else {
-        if (currentView > 1 ) {
-            currentView--
-        } else {
-            currentView = 3
-        }
+      currentView = 0;
     }
-    const images = document.querySelectorAll(".configurator-left > img");
-    images.forEach(element => {
-        let source = element.getAttribute("src");
-        source = source.substring(0, source.length - 5).concat(currentView + ".jpg");
-        element.setAttribute("src", source)
-    })
+  } else {
+    if (currentView > 0) {
+      currentView--;
+    } else {
+      currentView = 2;
+    }
+  }
+  const images = document.querySelectorAll('.img-wrapper > img');
+  images.forEach((element) => {
+    let source = element.getAttribute('src');
+    source = source.substring(0, source.length - 5).concat(currentView + '.png');
+    element.setAttribute('src', source);
+  });
+}
+
+function showElement(id) {
+  const element = document.getElementById(id);
+  if (element.style.visibility == 'hidden') {
+    element.style.visibility = 'initial';
+  } else {
+    element.style.visibility = 'hidden';
+  }
 }
