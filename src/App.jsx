c46eb8c1f3babbe28a995ff57ee41b5de41d8d27
KovacsLevente199 <kovacslevente199@gmail.com>
2023-02-23 19:27:25 +0100

    Fixed name issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import style from './App.css';
-import BoatDisplay from './components/BoatDisplay/BoatDisplay.jsx.js';
-import Header from './components/Header/Header.jsx.js';
+import BoatDisplay from './components/BoatDisplay/BoatDisplay.jsx';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
