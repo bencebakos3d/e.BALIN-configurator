efbb02830690ca98f43eed75f9957ca05d8e27f0
Bence Bakos <58910140+bencebakos3d@users.noreply.github.com>
2023-02-24 15:03:27 +0100

    Added ebalin tura model and viewer

--- src/components/BoatDisplay/BoatDisplay.jsx
+++ src/components/BoatDisplay/BoatDisplay.jsx
@@ -1,10 +1,25 @@
 import { Canvas } from '@react-three/fiber';
 import styles from './BoatDisplay.module.css';
+import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useLoader } from '@react-three/fiber';
+const BoatModel = { title: 'Boat', url: './model/ebalin_tura.gltf' };
+
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  return <primitive object={scene} />;
+}
 
 export default function BoatDisplay() {
   return (
     <div className={styles.boat_display}>
-      <Canvas></Canvas>
+      <Canvas camera={{ fov: 45, position: [2, 2, -5] }}>
+        <group>
+          <Model url={BoatModel.url} />
+        </group>
+        <OrbitControls enablePan={false} minDistance={1.8} maxDistance={8} maxPolarAngle={Math.PI / 2} enableDamping={false} />
+        <Environment files="./env/kloppenheim_05_puresky_1k.hdr" />
+      </Canvas>
     </div>
   );
 }
