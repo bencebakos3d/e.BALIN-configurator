3e6741b44a62028e8dd8a664ba44289bd2cd2ede
KovacsLevente199 <kovacslevente199@gmail.com>
2023-03-03 11:10:26 +0100

    Added dynamic page mainpage loading

--- src/components/ConfigurationPanel/ConfigurationPanel.jsx
+++ src/components/ConfigurationPanel/ConfigurationPanel.jsx
@@ -1,5 +1,7 @@
 import styles from './ConfigurationPanel.module.css';
 
+import { useState } from 'react';
+
 import boatBody from './icons/hajotest_icon.png';
 import carpet from './icons/karpit_icon.png';
 import electronics from './icons/elektronika_icon.png';
@@ -10,11 +12,13 @@ import arrow_right from './icons/arrow_right_icon.png';
 import bimini from './icons/bimini.png'
 import checked_icon from './icons/checked_icon.png'
 
-function NavBar() {
+import entry_data from './icons/ebalin_tura.json'
+
+function NavBar(handler) {
   return (
     <div className={styles.icons_bar}>
       <div className={styles.button_parent}>
-        <div className={styles.nav_button}>
+        <div className={styles.nav_button} onClick={()=>handler.handler("body")}>
           <img className={styles.image_sizing} src={boatBody} alt="" />
         </div>
         <div className={styles.button_label}>
@@ -23,7 +27,7 @@ function NavBar() {
       </div>
 
       <div className={styles.button_parent}>
-        <div className={styles.nav_button}>
+        <div className={styles.nav_button} onClick={()=>handler.handler("carpet")}>
           <img className={styles.image_sizing} src={carpet} alt="" />
         </div>
         <div className={styles.button_label}>
@@ -32,7 +36,7 @@ function NavBar() {
       </div>
 
       <div className={styles.button_parent}>
-        <div className={styles.nav_button}>
+        <div className={styles.nav_button} onClick={()=>handler.handler("electronics")}>
           <img className={styles.image_sizing} src={electronics} alt="" />
         </div>
         <div className={styles.button_label}>
@@ -41,7 +45,7 @@ function NavBar() {
       </div>
 
       <div className={styles.button_parent}>
-        <div className={styles.nav_button}>
+        <div className={styles.nav_button} onClick={()=>handler.handler("accessories")}>
           <img className={styles.image_sizing} src={accessories} alt="" />
         </div>
         <div className={styles.button_label}>
@@ -50,7 +54,7 @@ function NavBar() {
       </div>
 
       <div className={styles.button_parent}>
-        <div className={styles.nav_button}>
+        <div className={styles.nav_button} onClick={()=>handler.handler("storage")}>
           <img className={styles.image_sizing} src={storage} alt="" />
         </div>
         <div className={styles.button_label}>
@@ -61,7 +65,7 @@ function NavBar() {
   );
 }
 
-function Entry(){
+function Entry({dataRef}){
   return(
     <div className={styles.entry_mainframe}>
       <div className={styles.checkbox_container}>
@@ -71,10 +75,10 @@ function Entry(){
       </div>
       <div className={styles.text_pair}>
         <div className={styles.entry_title}>
-          Bimini
+          {dataRef.title}
         </div>
         <div>
-          Ideális nap és egyéb időjárási körülmények ellen.
+          {dataRef.details}
         </div>
       </div>
       <div className={styles.image_container}>
@@ -83,10 +87,32 @@ function Entry(){
     </div>
   );
 }
+
+function generateMainPage(category_name){
+  let filtered = new Array();
+
+  for(let element of entry_data){
+    if(element.category == category_name){
+      filtered.push(element);
+    }
+  }
+
+  return filtered.map((element,key)=>(
+        <Entry dataRef={element} id={key}/>
+  ))
+}
+
+
 export default function ConfigurationPanel() {
+  const [entry_list,setList] = useState([]);
+  const switchPage = (name)=>{
+    console.log(entry_list);
+    setList(entry_list.concat(generateMainPage(name)))
+  };
+  
   return (
     <div className={styles.menu_container}>
-      <NavBar></NavBar>
+      <NavBar handler={switchPage}></NavBar>
       <div className={styles.main_frame}>
         <div className={styles.title_bar}>
           Meg kell irni a Scriptet
@@ -97,19 +123,18 @@ export default function ConfigurationPanel() {
             <img src={arrow_left} className={styles.arrow_image} alt="" />
           </div>
           <div className={styles.content_zone}>
-            <Entry></Entry>
+            {entry_list}
           </div>
           <div className={styles.arrow_button}>
             <img className={styles.arrow_image} src={arrow_right} alt="" />
           </div>
         </div>
         <div className={styles.page_footer}>
-          <div className={styles.round_index}></div>
-          <div className={styles.round_index}></div>
-          <div className={styles.round_index}></div>
-          <div className={styles.round_index}></div>
-          <div className={styles.round_index}></div>
-          <div className={styles.round_index}></div>
+          <div className={styles.round_index} onClick={()=>switchPage("body")}></div>
+          <div className={styles.round_index} onClick={()=>switchPage("carpet")}></div>
+          <div className={styles.round_index} onClick={()=>switchPage("electronics")}></div>
+          <div className={styles.round_index} onClick={()=>switchPage("accessories")}></div>
+          <div className={styles.round_index} onClick={()=>switchPage("storage")}></div>
         </div>
       </div>
     </div>
