4bf12eb52230d64289b42bc875ab79b8dc9ed7a1
Bence Bakos <bencemail1@gmail.com>
2023-04-30 11:53:30 +0200

    Fixed navlink

--- src/components/ConfigurationPanel/ConfigurationPanel.jsx
+++ src/components/ConfigurationPanel/ConfigurationPanel.jsx
@@ -17,19 +17,19 @@ export default function ConfigurationPanel({ model }) {
       <div className={styles.main_frame}>
         <Routes>
           <Route exact path="/" element={<Hajotest model={model} />} />
-          <Route path={`karpit`} element={<Karpit />} />
-          <Route path={`elektronika`} element={<Elektronika />} />
-          <Route path="/kiegeszitok" element={<Kiegeszitok />} />
-          <Route path="/tarolas" element={<Tarolas />} />
-          <Route path="/osszesites" element={<Osszesites />} />
+          <Route path="karpit" element={<Karpit />} />
+          <Route path="elektronika" element={<Elektronika />} />
+          <Route path="kiegeszitok" element={<Kiegeszitok />} />
+          <Route path="tarolas" element={<Tarolas />} />
+          <Route path="osszesites" element={<Osszesites />} />
         </Routes>
         <div className={styles.page_footer}>
-          <NavLink to="/" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
-          <NavLink to="/karpit" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
-          <NavLink to="/elektronika" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
-          <NavLink to="/kiegeszitok" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
-          <NavLink to="/tarolas" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
-          <NavLink to="/osszesites" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="karpit" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="elektronika" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="kiegeszitok" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="tarolas" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
+          <NavLink to="osszesites" className={({ isActive }) => (isActive ? styles.round_index_active : styles.round_index)}></NavLink>
         </div>
       </div>
     </div>
